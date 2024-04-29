e0a2866e420e9ba8c7d03fdeca3fb615d544a0b3
Aditya <161931434+AdityaPatadiya@users.noreply.github.com>
2024-04-29 23:00:25 +0530

    Add files via upload

---
+++ pong_game/ball.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+        self.x_move = 10
+        self.y_move = 10
+        self.move_speed = 0.1
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce_y(self):
+        # after collision, we have to change the direction of the ball to the reverse direction.
+        self.y_move *= -1  # bcz if it is negative then we have to move to the positive side and if it is positive then
+        # we have to move to the negative side.
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.9
+
+    def reset_position(self):
+        self.goto(0, 0)
+        self.move_speed = 0.1
+        self.bounce_x()
